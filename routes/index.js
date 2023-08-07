e759f2d5cbe62d50f34f30e8c43bbe3fe3c30340
as6325400 <as6325400@gmail.com>
2023-08-07 17:44:23 +0800

    add server information

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const os = require('os');
+const osUtils = require('os-utils');
+const ejs = require('ejs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+function getCpuUsage() {
+  return new Promise((resolve, reject) => {
+    osUtils.cpuUsage((cpuUsage) => {
+      resolve(cpuUsage.toFixed(2));
+    });
+  });
+}
+
+async function getSystemInfo() {
+  const cpuUsage = await getCpuUsage();
+  const totalDiskSpace = os.totalmem();
+  const freeDiskSpace = os.freemem();
+  const usedDiskSpace = totalDiskSpace - freeDiskSpace;
+  const memoryUsage = ((totalDiskSpace - freeDiskSpace) / totalDiskSpace) * 100;
+  return { cpuUsage, usedDiskSpace, totalDiskSpace, memoryUsage };
+}
+
+router.get('/',async function(req, res, next) {
+  getSystemInfo()
+    .then((systemInfo) => {
+      let size = Number(1024 * 1024 * 1024);
+      systemInfo.usedDiskSpace = (Number(systemInfo.usedDiskSpace) / size).toFixed(2);
+      systemInfo.totalDiskSpace = (Number(systemInfo.totalDiskSpace) / size).toFixed(2);
+      systemInfo.memoryUsage = Number(systemInfo.memoryUsage).toFixed(2);
+      res.render('index', systemInfo);
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
 module.exports = router;
