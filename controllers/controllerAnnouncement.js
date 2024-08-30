af1feaa47f94644cc194084da5b8d2f6f0ba54c9
as6325400 <as6325400@gmail.com>
2024-08-30 18:16:45 +0800

    add edit version history

--- controllers/controllerAnnouncement.js
+++ controllers/controllerAnnouncement.js
@@ -23,6 +23,24 @@ const controller = {
           }
         });
       });
+    },
+
+    async editVersionHistory(_content){
+      let content = String(_content);
+      const title_id = 1;
+      return new Promise((resolve, reject) => {
+        database.query(
+          `INSERT INTO \`contents\`(\`title_id\`, \`content\`) VALUES (?,?);`, 
+          [title_id, content], (err, result, fields) => {
+          if (err) {
+            console.error('Error executing query:', err);
+            resolve(false);
+          }
+          else{
+            resolve(true);
+          }
+        });
+      });
     }
 };
 
--- routes/routerAnnouncement.js
+++ routes/routerAnnouncement.js
@@ -8,6 +8,12 @@ router.get('/version_histor', async function (req, res) {
   res.send(result);
 });
 
+router.post('/edit_version_histor', async function (req, res) {
+  const content = req.body.content;
+  let result = await controller.editVersionHistory(content);
+  res.send(result);
+});
+
 
 module.exports = router;
 
