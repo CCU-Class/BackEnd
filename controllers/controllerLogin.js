d18f9312be99ef19f9bcedcce75b7d9355187ee7
as6325400 <as6325400@gmail.com>
2024-08-30 16:24:31 +0800

    change otp login

--- controllers/controllerLogin.js
+++ controllers/controllerLogin.js
@@ -2,22 +2,23 @@ var modelAccount = require('../model/ModelAccount.js');
 var argon2 = require('argon2');
 
 const controller = {
-    async login(username, password){
-        let result = await this.varifyAccount(username, password);
+    async login(otp){
+        let result = await this.varifyAccount(otp);
         if(!result)return false;
         //更新登入時間
-        await this.updateLoginTime(username);
+        await this.updateLoginTime('admin');
         return true;
     },
-    async varifyAccount(username, password){
-        let result = await modelAccount.getAccount(username);
-        if(!result)return false;
-        if(process.env.USING_DATABASE== "postgre") result = result.rows;
+    async varifyAccount(otp){
+        // let result = await modelAccount.getAccount(username);
+        // if(!result)return false;
+        // if(process.env.USING_DATABASE== "postgre") result = result.rows;
         //驗證密碼
-        if(result.length == 0)return false;
-        const tureHash = result[0].password_hash;
-        succ = await argon2.verify(tureHash, password);
+        // if(result.length == 0)return false;
+        // const tureHash = result[0].password_hash;
+        // succ = await argon2.verify(tureHash, password);
         //console.log(succ);
+        const succ = await modelAccount.verifyOTP(otp);
         if(!succ)return false;
         return  true;
     },
